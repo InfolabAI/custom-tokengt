b3396dd189302c569f156459a2fc78b49e783d9e
heechul <skyde1021@dgist.ac.kr>
2023-05-12 07:34:26 +0000

    ref

--- large-scale-regression/tokengt/models/tokengt.py
+++ large-scale-regression/tokengt/models/tokengt.py
@@ -46,46 +46,159 @@ class TokenGTModel(FairseqEncoderModel):
     @staticmethod
     def add_args(parser):
         """Add model-specific arguments to the parser."""
-        parser.add_argument("--dropout", type=float, metavar="D",  help="dropout prob")
-        parser.add_argument("--attention-dropout", type=float, metavar="D", help="dropout prob for attention weights")
-        parser.add_argument("--act-dropout", type=float, metavar="D", help="dropout prob after activation in FFN")
+        parser.add_argument("--dropout", type=float, metavar="D", help="dropout prob")
+        parser.add_argument(
+            "--attention-dropout",
+            type=float,
+            metavar="D",
+            help="dropout prob for attention weights",
+        )
+        parser.add_argument(
+            "--act-dropout",
+            type=float,
+            metavar="D",
+            help="dropout prob after activation in FFN",
+        )
 
-        parser.add_argument("--encoder-ffn-embed-dim", type=int, metavar="N", help="encoder embedding dim for FFN")
-        parser.add_argument("--encoder-layers", type=int, metavar="N", help="num encoder layers")
-        parser.add_argument("--encoder-attention-heads", type=int, metavar="N", help="num encoder attention heads")
-        parser.add_argument("--encoder-embed-dim", type=int, metavar="N", help="encoder embedding dimension")
-        parser.add_argument("--share-encoder-input-output-embed", action="store_true",
-                            help="share encoder input and output embeddings")
+        parser.add_argument(
+            "--encoder-ffn-embed-dim",
+            type=int,
+            metavar="N",
+            help="encoder embedding dim for FFN",
+        )
+        parser.add_argument(
+            "--encoder-layers", type=int, metavar="N", help="num encoder layers"
+        )
+        parser.add_argument(
+            "--encoder-attention-heads",
+            type=int,
+            metavar="N",
+            help="num encoder attention heads",
+        )
+        parser.add_argument(
+            "--encoder-embed-dim",
+            type=int,
+            metavar="N",
+            help="encoder embedding dimension",
+        )
+        parser.add_argument(
+            "--share-encoder-input-output-embed",
+            action="store_true",
+            help="share encoder input and output embeddings",
+        )
 
-        parser.add_argument("--rand-node-id", action="store_true", help="use random feature node identifiers")
-        parser.add_argument("--rand-node-id-dim", type=int, metavar="N", help="dim of random node identifiers")
-        parser.add_argument("--orf-node-id", action="store_true", help="use orthogonal random feature node identifiers")
-        parser.add_argument("--orf-node-id-dim", type=int, metavar="N", help="dim of orthogonal random node identifier")
-        parser.add_argument("--lap-node-id", action="store_true", help="use Laplacian eigenvector node identifiers")
-        parser.add_argument("--lap-node-id-k", type=int, metavar="N",
-                            help="number of Laplacian eigenvectors to use, from smallest eigenvalues")
-        parser.add_argument("--lap-node-id-sign-flip", action="store_true", help="randomly flip the signs of eigvecs")
-        parser.add_argument("--lap-node-id-eig-dropout", type=float, metavar="D", help="dropout prob for Lap eigvecs")
-        parser.add_argument("--type-id", action="store_true", help="use type identifiers")
+        parser.add_argument(
+            "--rand-node-id",
+            action="store_true",
+            help="use random feature node identifiers",
+        )
+        parser.add_argument(
+            "--rand-node-id-dim",
+            type=int,
+            metavar="N",
+            help="dim of random node identifiers",
+        )
+        parser.add_argument(
+            "--orf-node-id",
+            action="store_true",
+            help="use orthogonal random feature node identifiers",
+        )
+        parser.add_argument(
+            "--orf-node-id-dim",
+            type=int,
+            metavar="N",
+            help="dim of orthogonal random node identifier",
+        )
+        parser.add_argument(
+            "--lap-node-id",
+            action="store_true",
+            help="use Laplacian eigenvector node identifiers",
+        )
+        parser.add_argument(
+            "--lap-node-id-k",
+            type=int,
+            metavar="N",
+            help="number of Laplacian eigenvectors to use, from smallest eigenvalues",
+        )
+        parser.add_argument(
+            "--lap-node-id-sign-flip",
+            action="store_true",
+            help="randomly flip the signs of eigvecs",
+        )
+        parser.add_argument(
+            "--lap-node-id-eig-dropout",
+            type=float,
+            metavar="D",
+            help="dropout prob for Lap eigvecs",
+        )
+        parser.add_argument(
+            "--type-id", action="store_true", help="use type identifiers"
+        )
 
-        parser.add_argument("--stochastic-depth", action="store_true", help="use stochastic depth regularizer")
+        parser.add_argument(
+            "--stochastic-depth",
+            action="store_true",
+            help="use stochastic depth regularizer",
+        )
 
-        parser.add_argument("--performer", action="store_true", help="linearized self-attention with Performer kernel")
-        parser.add_argument("--performer-nb-features", type=int, metavar="N",
-                            help="number of random features for Performer, defaults to (d*log(d)) where d is head dim")
-        parser.add_argument("--performer-feature-redraw-interval", type=int, metavar="N",
-                            help="how frequently to redraw the projection matrix for Performer")
-        parser.add_argument("--performer-generalized-attention", action="store_true",
-                            help="defaults to softmax approximation, but can be set to True for generalized attention")
-        parser.add_argument("--performer-finetune", action="store_true",
-                            help="load softmax checkpoint and fine-tune with performer")
+        parser.add_argument(
+            "--performer",
+            action="store_true",
+            help="linearized self-attention with Performer kernel",
+        )
+        parser.add_argument(
+            "--performer-nb-features",
+            type=int,
+            metavar="N",
+            help="number of random features for Performer, defaults to (d*log(d)) where d is head dim",
+        )
+        parser.add_argument(
+            "--performer-feature-redraw-interval",
+            type=int,
+            metavar="N",
+            help="how frequently to redraw the projection matrix for Performer",
+        )
+        parser.add_argument(
+            "--performer-generalized-attention",
+            action="store_true",
+            help="defaults to softmax approximation, but can be set to True for generalized attention",
+        )
+        parser.add_argument(
+            "--performer-finetune",
+            action="store_true",
+            help="load softmax checkpoint and fine-tune with performer",
+        )
 
-        parser.add_argument("--apply-graphormer-init", action="store_true", help="use Graphormer initialization")
-        parser.add_argument("--activation-fn", choices=utils.get_available_activation_fns(), help="activation to use")
-        parser.add_argument("--encoder-normalize-before", action="store_true", help="apply layernorm before encoder")
-        parser.add_argument("--prenorm", action="store_true", help="apply layernorm before self-attention and ffn")
-        parser.add_argument("--postnorm", action="store_true", help="apply layernorm after self-attention and ffn")
-        parser.add_argument("--return-attention", action="store_true", help="obtain attention maps from all layers",)
+        parser.add_argument(
+            "--apply-graphormer-init",
+            action="store_true",
+            help="use Graphormer initialization",
+        )
+        parser.add_argument(
+            "--activation-fn",
+            choices=utils.get_available_activation_fns(),
+            help="activation to use",
+        )
+        parser.add_argument(
+            "--encoder-normalize-before",
+            action="store_true",
+            help="apply layernorm before encoder",
+        )
+        parser.add_argument(
+            "--prenorm",
+            action="store_true",
+            help="apply layernorm before self-attention and ffn",
+        )
+        parser.add_argument(
+            "--postnorm",
+            action="store_true",
+            help="apply layernorm after self-attention and ffn",
+        )
+        parser.add_argument(
+            "--return-attention",
+            action="store_true",
+            help="obtain attention maps from all layers",
+        )
 
     def max_nodes(self):
         return self.encoder.max_nodes
@@ -105,6 +218,7 @@ class TokenGTModel(FairseqEncoderModel):
         return cls(args, encoder)
 
     def forward(self, batched_data, **kwargs):
+        breakpoint()
         return self.encoder(batched_data, **kwargs)
 
 
@@ -175,8 +289,12 @@ class TokenGTEncoder(FairseqEncoder):
 
         # Remove head is set to true during fine-tuning
         self.load_softmax = not getattr(args, "remove_head", False)
-        self.masked_lm_pooler = nn.Linear(args.encoder_embed_dim, args.encoder_embed_dim)
-        self.lm_head_transform_weight = nn.Linear(args.encoder_embed_dim, args.encoder_embed_dim)
+        self.masked_lm_pooler = nn.Linear(
+            args.encoder_embed_dim, args.encoder_embed_dim
+        )
+        self.lm_head_transform_weight = nn.Linear(
+            args.encoder_embed_dim, args.encoder_embed_dim
+        )
         self.activation_fn = utils.get_activation_fn(args.activation_fn)
         self.layer_norm = LayerNorm(args.encoder_embed_dim)
 
@@ -184,7 +302,9 @@ class TokenGTEncoder(FairseqEncoder):
         if self.load_softmax:
             self.lm_output_learned_bias = nn.Parameter(torch.zeros(1))
             if not self.share_input_output_embed:
-                self.embed_out = nn.Linear(args.encoder_embed_dim, args.num_classes, bias=False)
+                self.embed_out = nn.Linear(
+                    args.encoder_embed_dim, args.num_classes, bias=False
+                )
             else:
                 raise NotImplementedError
 
@@ -194,7 +314,9 @@ class TokenGTEncoder(FairseqEncoder):
             self.embed_out.reset_parameters()
 
     def forward(self, batched_data, perturb=None, masked_tokens=None, **unused):
-        inner_states, graph_rep, attn_dict = self.graph_encoder(batched_data, perturb=perturb)
+        inner_states, graph_rep, attn_dict = self.graph_encoder(
+            batched_data, perturb=perturb
+        )
 
         x = inner_states[-1].transpose(0, 1)  # B x T x C
 
@@ -206,7 +328,7 @@ class TokenGTEncoder(FairseqEncoder):
 
         # project back to size of vocabulary
         if self.share_input_output_embed and hasattr(
-                self.graph_encoder.embed_tokens, "weight"
+            self.graph_encoder.embed_tokens, "weight"
         ):
             x = F.linear(x, self.graph_encoder.embed_tokens.weight)
         elif self.embed_out is not None:
@@ -247,7 +369,9 @@ def base_architecture(args):
     args.activation_fn = getattr(args, "activation_fn", "gelu")
     args.encoder_normalize_before = getattr(args, "encoder_normalize_before", True)
     args.apply_graphormer_init = getattr(args, "apply_graphormer_init", True)
-    args.share_encoder_input_output_embed = getattr(args, "share_encoder_input_output_embed", False)
+    args.share_encoder_input_output_embed = getattr(
+        args, "share_encoder_input_output_embed", False
+    )
     args.prenorm = getattr(args, "prenorm", False)
     args.postnorm = getattr(args, "postnorm", False)
 
@@ -266,8 +390,12 @@ def base_architecture(args):
     args.performer = getattr(args, "performer", False)
     args.performer_finetune = getattr(args, "performer_finetune", False)
     args.performer_nb_features = getattr(args, "performer_nb_features", None)
-    args.performer_feature_redraw_interval = getattr(args, "performer_feature_redraw_interval", 1000)
-    args.performer_generalized_attention = getattr(args, "performer_generalized_attention", False)
+    args.performer_feature_redraw_interval = getattr(
+        args, "performer_feature_redraw_interval", 1000
+    )
+    args.performer_generalized_attention = getattr(
+        args, "performer_generalized_attention", False
+    )
 
     args.return_attention = getattr(args, "return_attention", False)
 
@@ -281,11 +409,13 @@ def tokengt_base_architecture(args):
     args.dropout = getattr(args, "dropout", 0.0)
     args.attention_dropout = getattr(args, "attention_dropout", 0.1)
     args.act_dropout = getattr(args, "act_dropout", 0.1)
-    
+
     args.activation_fn = getattr(args, "activation_fn", "gelu")
     args.encoder_normalize_before = getattr(args, "encoder_normalize_before", True)
     args.apply_graphormer_init = getattr(args, "apply_graphormer_init", True)
-    args.share_encoder_input_output_embed = getattr(args, "share_encoder_input_output_embed", False)
+    args.share_encoder_input_output_embed = getattr(
+        args, "share_encoder_input_output_embed", False
+    )
     args.prenorm = getattr(args, "prenorm", False)
     args.postnorm = getattr(args, "postnorm", False)
 
@@ -304,8 +434,12 @@ def tokengt_base_architecture(args):
     args.performer = getattr(args, "performer", False)
     args.performer_finetune = getattr(args, "performer_finetune", False)
     args.performer_nb_features = getattr(args, "performer_nb_features", None)
-    args.performer_feature_redraw_interval = getattr(args, "performer_feature_redraw_interval", 1000)
-    args.performer_generalized_attention = getattr(args, "performer_generalized_attention", False)
+    args.performer_feature_redraw_interval = getattr(
+        args, "performer_feature_redraw_interval", 1000
+    )
+    args.performer_generalized_attention = getattr(
+        args, "performer_generalized_attention", False
+    )
 
     args.return_attention = getattr(args, "return_attention", False)
     base_architecture(args)
@@ -324,7 +458,9 @@ def tokengt_base_ablated_architecture(args):
     args.activation_fn = getattr(args, "activation_fn", "gelu")
     args.encoder_normalize_before = getattr(args, "encoder_normalize_before", True)
     args.apply_graphormer_init = getattr(args, "apply_graphormer_init", True)
-    args.share_encoder_input_output_embed = getattr(args, "share_encoder_input_output_embed", False)
+    args.share_encoder_input_output_embed = getattr(
+        args, "share_encoder_input_output_embed", False
+    )
     args.prenorm = getattr(args, "prenorm", False)
     args.postnorm = getattr(args, "postnorm", False)
 
@@ -343,8 +479,12 @@ def tokengt_base_ablated_architecture(args):
     args.performer = getattr(args, "performer", False)
     args.performer_finetune = getattr(args, "performer_finetune", False)
     args.performer_nb_features = getattr(args, "performer_nb_features", None)
-    args.performer_feature_redraw_interval = getattr(args, "performer_feature_redraw_interval", 1000)
-    args.performer_generalized_attention = getattr(args, "performer_generalized_attention", False)
+    args.performer_feature_redraw_interval = getattr(
+        args, "performer_feature_redraw_interval", 1000
+    )
+    args.performer_generalized_attention = getattr(
+        args, "performer_generalized_attention", False
+    )
 
     args.return_attention = getattr(args, "return_attention", False)
     base_architecture(args)
